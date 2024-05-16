6b3ed5ddbcfd42d363955e988e4921a13ff2b1cf
woo-be <thsckddn100@gmail.com>
2024-05-16 12:24:11 +0900

    b

--- app/src/main/java/com/moyeo/App.java
+++ app/src/main/java/com/moyeo/App.java
@@ -23,8 +23,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 @SpringBootApplication
 @EnableTransactionManagement
 @PropertySource({
-    "classpath:config/ncp.properties",
-    "classpath:config/ncp-secret.properties"
+    "file:${user.home}/config/jdbc.properties",
+    "file:${user.home}/config/ncp.properties",
+    "file:${user.home}/config/ncp-secret.properties"
 })
 @Controller
 public class App {
